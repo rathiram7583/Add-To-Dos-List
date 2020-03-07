8b0eb7cc6d3b600941438ec9094582945dfce85a
668Name <66688@example.com>
2020-03-06 18:58:13 -0700

    added

--- js/To-Do-List.js
+++ js/To-Do-List.js
@@ -7,20 +7,60 @@
    addToDo.addEventListener('click', function(event){
 
     
-         if(enterTask.value.length>0)
-            {
-               
-                var checkBox=document.createElement("INPUT");
-                checkBox.setAttribute("type", "checkbox",label);
-                 checkBox.textContent=enterTask.value; 
-                activeToDo.appendChild(checkBox);
+    if(enterTask.value.length>0)
+          {
 
                  var li = document.createElement("li");
-                 li.textContent=enterTask.value;
+                 var checkBox=document.createElement("INPUT");
+                 checkBox.setAttribute("type", "checkbox");
+                 li.appendChild(checkBox);
+                 li.innerHTML+=enterTask.value;
                  activeToDo.appendChild(li);
+          }
+        
+
+               var checkValue=document.querySelector('#activetodo li:last-child input');
+               checkValue.addEventListener('click',function(event)
+                {
+                  var parentLi=this.parentNode;
+                  console.log(parentLi==true);
+                
+                  if(parentLi != undefined )
+                 {
+                   
+                  var li1=document.createElement("li");
+                  var checkBox1=document.createElement("INPUT");
+                  checkBox1.setAttribute("type","checkbox");
+                  li1.appendChild(checkBox1);
+                  li1.innerHTML+=parentLi.textContent;
+                  completed.appendChild(li1);
+
+                
+                }
+
+                  
+                });
 
 
-            }
+              });
+                
+
+          
+          
 
 
- });
+               
+
+
+              
+                
+                 
+            
+          
+        
+
+
+
+
+
+
