a42ce809a37415a58a0e26af26a138c1b4a56da7
Rathi <rathiram7583@gmail.com>
2020-03-08 19:05:04 -0600

    Added

--- js/To-Do-List.js
+++ js/To-Do-List.js
@@ -1,48 +1,65 @@
  var enterTask =document.getElementById('enterTask');
  var addToDo=document.getElementById('addToDo');
  var activeToDo=document.getElementById('activetodo');
- var completed=document.getElementById('completed');
+ var completed=document.getElementById('complete');
+
+function add(input){
+
+    var input=enterTask;
+  if(input.value.length>0)
+  {
+
+
+         var li = document.createElement("li");
+         var checkBox=document.createElement("INPUT");
+         checkBox.setAttribute("type", "checkbox");
+         li.appendChild(checkBox);
+         
+         li.innerHTML+=input.value;
+         activeToDo.appendChild(li);
+  }
+    input.value="";
+}
+
+ 
 
  
    addToDo.addEventListener('click', function(event){
 
     
-    if(enterTask.value.length>0)
-          {
+              add(enterTask);
+          
 
-                 var li = document.createElement("li");
-                 var checkBox=document.createElement("INPUT");
-                 checkBox.setAttribute("type", "checkbox");
-                 li.appendChild(checkBox);
-                 li.innerHTML+=enterTask.value;
-                 activeToDo.appendChild(li);
-          }
-        
-
-               var checkValue=document.querySelector('#activetodo li:last-child input');
+              var checkValue=document.querySelector('#activetodo li:last-child input');
+ 
                checkValue.addEventListener('click',function(event)
                 {
-                  var parentLi=this.parentNode;
-                  console.log(parentLi==true);
+                  
+              
+ 
+               
+                  var completeInput=this.parentNode;
+                  console.log(completeInput==true);
                 
-                  if(parentLi != undefined )
+                  if(completeInput!= undefined )
                  {
                    
                   var li1=document.createElement("li");
                   var checkBox1=document.createElement("INPUT");
                   checkBox1.setAttribute("type","checkbox");
                   li1.appendChild(checkBox1);
-                  li1.innerHTML+=parentLi.textContent;
+                  li1.innerHTML+=completeInput.textContent;
                   completed.appendChild(li1);
+                  this. parentNode.removeChild(element.parentNode);
 
                 
                 }
-
-                  
+   
+ 
                 });
-
-
               });
+
+            
                 
 
           
